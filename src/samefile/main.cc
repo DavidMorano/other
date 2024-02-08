5b06c85228916a2144b25147f1d394e71a2af84a
David A.D. Morano <david.a.morano@gmail.com>
2024-02-08 10:41:47 -0500

    comments |main|
    ok

--- src/samefile/main.cc
+++ src/samefile/main.cc
@@ -1,5 +1,8 @@
-/* main */
+/* main SUPPORT */
 /* lang=C++23 */
+
+/* determine if two files are actually the same (but dev-ino comparison) */
+/* version %I% last-modified %G% */
 
 
 /* revision history:
@@ -13,17 +16,16 @@
 
 /*******************************************************************************
 
-	This program determines if the two named files (given as two arguments)
-	are the same file or not (based on equality of 'device' and 'inode'
-	numbers.
-
+	This program determines if the two named files (given as
+	two arguments) are the same file or not (based on equality
+	of 'device' and 'inode' numbers.
 
 *******************************************************************************/
-
 
 #include	<sys/types.h>
 #include	<sys/stat.h>
 #include	<unistd.h>
+#include	<cstdlib>		/* |EXIT_SUCCESS| */
 #include	<libgen.h>
 #include	<iostream>
 
@@ -34,13 +36,14 @@ using namespace		std ;
 
 /* typedefs */
 
-typedef const char	cchar ;
-typedef struct stat	statblock ;
+typedef const char *const *	mainv ;
+typedef const char		cchar ;
+typedef struct stat		statblock ;
 
 /* exported subroutines */
 
-int main(int argc,cchar **argv,cchar **) noexcept {
-	int	ex = 1 ;
+int main(int argc,mainv argv,mainv) noexcept {
+	int		ex = EXIT_FAILURE ;
 	if (argc > 0) {
 	    cchar	*pn = basename(const_cast<char *>(argv[0])) ;
 	    if ((argc == 3) && (argv != nullptr)) {
@@ -51,12 +54,12 @@ int main(int argc,cchar **argv,cchar **) noexcept {
 		    if (stat(fn2,&sb2) >= 0) {
 			bool	f = (sb1.st_dev == sb2.st_dev) ;
 			f = f && (sb1.st_ino == sb2.st_ino) ;
-			if (f) ex = 0 ;
+			if (f) ex = EXIT_SUCCESS ;
 		    } else {
-	                cerr << pn << ": cannot stat file\n" ;
+	                cerr << pn << ": cannot stat file (2)\n" ;
 		    }
 		} else {
-	            cerr << pn << ": cannot stat file\n" ;
+	            cerr << pn << ": cannot stat file (1)\n" ;
 		}
 	    } else {
 	        cerr << pn << ": not enough arguments\n" ;
