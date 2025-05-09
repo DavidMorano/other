103a7d07caf7f2ee1a7867b4494aa67abdd9fcca
David A.D. Morano <david.a.morano@gmail.com>
2025-05-09 00:06:15 -0400

    style

--- src/apple_telnetd/strlcpy.c
+++ src/apple_telnetd/strlcpy.c
@@ -45,14 +45,10 @@ static const char rcsid[] =
  * will be copied.  Always NUL terminates (unless siz == 0).
  * Returns strlen(src); if retval >= siz, truncation occurred.
  */
-size_t strlcpy(dst, src, siz)
-	char *dst;
-	const char *src;
-	size_t siz;
-{
-	register char *d = dst;
-	register const char *s = src;
-	register size_t n = siz;
+size_t strlcpy(char *dst,const char *src,size_t siz) {
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
 
 	/* Copy as many bytes as will fit */
 	if (n != 0 && --n != 0) {
@@ -70,5 +66,7 @@ size_t strlcpy(dst, src, siz)
 			;
 	}
 
-	return(s - src - 1);	/* count does not include NUL */
+	return (s - src - 1);	/* count does not include NUL */
 }
+
+
