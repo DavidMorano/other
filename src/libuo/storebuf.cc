f142cd73994f1b6cb89e384c99d2efe0d8e7b04f
David A.D. Morano <david.a.morano@gmail.com>
2025-06-13 00:15:48 -0400

    comments

--- src/libuo/storebuf.cc
+++ src/libuo/storebuf.cc
@@ -1,5 +1,5 @@
 /* storebuf SUPPORT */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* storage buffer manipulation subroutines */
--- src/libuo/strn.h
+++ src/libuo/strn.h
@@ -1,5 +1,5 @@
 /* strn HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* perform variable counted c-string operations */
