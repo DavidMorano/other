bc876d974af5aabda14d16bae34c071198e31224
David A.D. Morano <david.a.morano@gmail.com>
2025-07-22 01:10:32 -0400

    comments + update

--- src/machostid/gethostid.cc
+++ src/machostid/gethostid.cc
@@ -1,4 +1,5 @@
 /* gethostid SUPPORT */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* get the host-ID value (a 32-bit binary number) */
@@ -17,20 +18,29 @@
 
 /*******************************************************************************
 
+  	Name:
+	getostid
+
+	Description:
 	Retrieve the host ID value (a 32-bit unsigned binary integer
 	value).
 
 *******************************************************************************/
 
-#include	<envstandards.h>	/* first to configure */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>		/* required by |sysctl(3)| */
 #include	<sys/sysctl.h>		/* required by |sysctl(3)| */
 #include	<cerrno>
 #include	<cstddef>		/* <- |nullptr_t| ?? */
 #include	<cstdint>		/* <- |int32_t| */
 #include	<cstdio>
-#include	<utypedefs.h>
 #include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<localmisc.h>
 
 
 /* local defines */
@@ -47,14 +57,13 @@ using std::nullptr_t ;			/* type */
 /* exported subroutines */
 
 long gethostid() noex {
-	const nullptr_t	np{} ;
+	cnullptr	np{} ;
 	size_t		idlen = sizeof(int32_t) ;
 	int32_t		id{} ;
 	if (sysctlbyname("kern.hostid",&id,&idlen,np,0uz) < 0) {
-	printf("errno=%d id=%08x\n",errno,id) ;
+	    fprintf(stderr,"errno=%d id=%08x\n",errno,id) ;
 	    id = 0 ;
 	}
-	printf("errno=%d id=%08x\n",errno,id) ;
 	return long(id) ;
 }
 /* end subroutine (gethostid) */
