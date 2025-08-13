d0449ba2f190f35dfdb4a4d5dde9c74b52af2b49
David A.D. Morano <david.a.morano@gmail.com>
2025-08-13 02:09:59 -0400

    comments + update

--- src/macfiles/modproc/modproc1.cc
+++ src/macfiles/modproc/modproc1.cc
@@ -1,4 +1,4 @@
-/* modproc1 MODULE (module-implementation-unit) */
+/* loadmodnames1 MODULE (module-implementation-unit) */
 /* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
@@ -17,8 +17,23 @@
 
 /*******************************************************************************
 
-	Object:
-	modproc
+	Names:
+	loadmodnames
+
+	Description:
+	I parse a C++ source code file and find all of the module
+	names that I can that are being imported to rhat source file.
+
+	Synopsis:
+	loadmodnames(vecstr *op,cchar *fname) noex
+
+	Arguments:
+	op		ureserve::vecstr
+	fname		file-name to parse
+
+	Returns:
+	>=0		number of imported modules found
+	<0		error (system-return)
 
 *******************************************************************************/
 
@@ -31,15 +46,12 @@ module ;
 #include	<climits>		/* |INT_MAX| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<new>			/* placement-new + |nothrow(3c++)| */
-#include	<ostream>
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 #include	<usysrets.h>
 #include	<usyscalls.h>
-#include	<ulogerror.h>
 #include	<ascii.h>
 #include	<hasx.h>		/* |hasmodname(3uc)| */
 #include	<strop.h>
@@ -49,26 +61,18 @@ module ;
 #include	<localmisc.h>
 
 #pragma		GCC dependency	"mod/ureserve.ccm"
-#pragma		GCC dependency	"mod/loadmodnames.ccm"
 
-module modproc ;
+module loadmodnames ;
 
 import ureserve ;			/* |vecstr(3u)| */
-import loadmodnames ;
 
 /* local defines */
 
 
 /* imported namespaces */
 
-using std::nullptr_t ;			/* type */
-using std::ostream ;			/* type */
-using std::nothrow ;			/* constant */
-
 
 /* local typedefs */
-
-typedef vecstr *	vecstrp ;
 
 
 /* external subroutines */
@@ -79,11 +83,24 @@ typedef vecstr *	vecstrp ;
 
 /* local structures */
 
+namespace {
+    struct vecmgr {
+	vecstr		*vop ;
+	vecmgr(vecstr *p) noex : vop(p) { } ;
+	int operator () (cchar *) noex ;
+	int filenmods(cchar *) noex ;
+	int liner(int,size_t) noex ;
+	int procln(cchar *,int) noex ;
+    } ; /* end struct (vecmgr) */
+} /* end namespace */
+
 
 /* forward references */
 
 
 /* local variables */
+
+cchar			istr[] = "import" ;
 
 
 /* exported variables */
@@ -91,73 +108,110 @@ typedef vecstr *	vecstrp ;
 
 /* exported subroutines */
 
-int modproc::istart() noex {
-    	cnullptr	np{} ;
-    	int		rs = SR_NOMEM ;
-    	if (vecstr *vsp ; (vsp = new(nothrow) vecstr) != np) {
-	    vop = vsp ;
-	    rs = SR_OK ;
-	} /* end if (new-vecstr) */
-	return rs ;
-} /* end method (modproc::istart) */
-
-int modproc::ifinish() noex {
-    	int		rs = SR_NOTOPEN ;
-	if (vop)  {
-	    vecstr	*vsp = vecstrp(vop) ;
-	    rs = SR_OK ;
-	    {
-		delete vsp ;
-	    }
-	    vop = nullptr ;
+int loadmodnames(vecstr *op,cchar *fname) noex {
+    	int		rs = SR_FAULT ;
+	if (op && fname) ylikely {
+    	    vecmgr	mgr(op) ;
+	    rs = mgr(fname) ;
 	} /* end if (non-null) */
 	return rs ;
-} /* end method (modproc::ifinish) */
-
-int modproc::procfile(cchar *fn) noex {
-    	int		rs = SR_FAULT ;
-	int		c = 0 ;
-	if (fn) ylikely {
-	    if (fn[0]) ylikely {
-		vecstr *vsp = vecstrp(vop) ;
-		rs = loadmodnames(vsp,fn) ;
-		c = rs ;
-	    } /* end if (valid) */
-	} /* end if (non-null) */
-	return (rs >= 0) ? c : rs ;
-} /* end method (modproc::procfile) */
-
-int modproc::procout(ostream *osp,uint ot) noex {
-    	int		rs = SR_FAULT ;
-	if (osp) {
-	    rs = SR_OK ;
-	    (void) ot ;
-	}
-	return rs ;
-} /* end method (modproc::procout) */
+}
+/* end subroutine (loadmodname) */
 
 
 /* local subroutines */
 
-void modproc::dtor() noex {
-	if (cint rs = finish ; rs < 0) {
-	    ulogerror("modproc",rs,"fini-finish") ;
-	}
-} /* end method (modproc::dtor) */
+int vecmgr::operator () (cchar *fname) noex {
+    	return filenmods(fname) ;
+} /* end method (vecmgr::operator) */
 
-modproc_co::operator int () noex {
-	int		rs = SR_BUGCHECK ;
-	if (op) ylikely {
-	    switch (w) {
-	    case modprocmem_start:
-	        rs = op->istart() ;
-	        break ;
-	    case modprocmem_finish:
-	        rs = op->ifinish() ;
-	        break ;
-	    } /* end switch */
+int vecmgr::filenmods(cchar *fname) noex {
+	int		rs = SR_FAULT ;
+	int		rs1 ;
+	int		nmods = 0 ;
+	if (fname) ylikely {
+	    rs = SR_INVALID ;
+	    if (fname[0]) ylikely {
+		cint	of = O_RDONLY ;
+		if ((rs = u_open(fname,of,0)) >= 0) ylikely {
+		    cint	fd = rs ;
+		    if (ustat sb ; (rs = u_fstat(fd,&sb)) >= 0) ylikely {
+			csize	fsize = size_t(sb.st_size) ;
+			rs = SR_NOTSUP ;
+		        if (S_ISREG(sb.st_mode)) ylikely {
+			    rs = SR_OK ;
+			    if (fsize > 0) {
+			        rs = liner(fd,fsize) ;
+			        nmods = rs ;
+			    }
+			} /* end if (regular file) */
+		    } /* end if (stat) */
+		    rs1 = u_close(fd) ;
+		    if (rs >= 0) rs = rs1 ;
+		} /* end if (open) */
+	    } /* end if (valid) */
 	} /* end if (non-null) */
-	return rs ;
-} /* end method (modproc_co::operator) */
+	return (rs >= 0) ? nmods : rs ;
+} /* end method (vecmgr::filenmods) */
+
+int vecmgr::liner(int fd,csize ms) noex {
+	cint		mp = PROT_READ ;
+	cint		mf = MAP_SHARED ;
+	int		rs ;
+	int		rs1 ;
+	int		nmods = 0 ; /* return-value */
+	cnullptr	np{} ;
+	if (void *md ; (rs = u_mmapbegin(np,ms,mp,mf,fd,0z,&md)) >= 0) {
+	    cint	cmd = MADV_SEQUENTIAL ;
+	    if ((rs = u_madvise(md,ms,cmd)) >= 0) ylikely {
+		size_t	ll = ms ;
+		cchar	*lp = charp(md) ;
+		for (cchar *tp ; (tp = charp(memchr(lp,'\n',ll))) != np ; ) {
+		    csize	si = ((tp + 1) - lp) ;
+		    if (cint sl = intconv(si - 1) ; sl > 0) {
+		        rs = procln(lp,sl) ;
+		        nmods += rs ;
+		    }
+		    ll -= si ;
+		    lp += si ;
+		} /* end for */
+		if ((rs >= 0) && (ll > 0)) {
+		    cint	sl = intconv(ll) ;
+		    rs = procln(lp,sl) ;
+		    nmods += rs ;
+		} /* end if (trailing line */
+	    } /* end if (memory-advise) */
+	    rs1 = u_mmapend(md,ms) ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (map-file) */
+	return (rs >= 0) ? nmods : rs ;
+} /* end method (vecmgr::liner) */
+
+int vecmgr::procln(cchar *lp,int ll) noex {
+    	int		rs ;
+	int		rs1 ;
+	int		c = 0 ; /* return-value -- number of names found */
+    	if (strop s ; (rs = s.start(lp,ll)) >= 0) ylikely {
+	    cchar	*ip ;
+	    if (int il ; (il = s.fieldwht(&ip)) > 0) {
+		if (strwcmp(istr,ip,il) == 0) {
+		    cint	ch_s = CH_SEMI ;
+		    cchar	*mp ;
+		    if (int ml ; (ml = s.fieldchr(ch_s,&mp)) > 0) {
+		        s.white() ;
+			if (s.sp[0] == ch_s) {
+			    if (hasmodname(mp,ml)) {
+			        rs = vop->adduniq(mp,ml) ;
+			        c = (rs < INT_MAX) ;
+			    }
+			}
+		    }
+	        } /* end if (got "import") */
+	    } /* end if (got field) */
+	    rs1 = s.finish ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (strop) */
+	return (rs >= 0) ? c : rs ;
+} /* end method (vecmgr::procln) */
 
 
