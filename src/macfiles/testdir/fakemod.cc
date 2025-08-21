461983829337d2d004e24b620c7d8cf23c1a4039
David A.D. Morano <david.a.morano@gmail.com>
2025-08-21 00:15:57 -0400

    comments + update

--- src/macfiles/testdir/fakemod.cc
+++ src/macfiles/testdir/fakemod.cc
@@ -1,6 +1,22 @@
 /* code w/ fake module import (for testing)  */
 
+#include	<envstandards.h>	/* must be ordered first to configure */
 
 import libutil ;
+
+import /* here is a thing */
+/* and another */
+chrset
+/* finally another */
+;
+
+import    	   vecbool;
+
+/* comment 
+   import ureserve ;
+
+   more */
+
+
 
 
