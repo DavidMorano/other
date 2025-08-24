5d5f0da4f119dae9949d4b076fe0122abcbcbd07
David A.D. Morano <david.a.morano@gmail.com>
2025-08-24 00:11:31 -0400

    comments + update

--- src/macfiles/modproc/modproc1.cc
+++ src/macfiles/modproc/modproc1.cc
@@ -35,6 +35,14 @@
 	>=0		number of imported modules found
 	<0		error (system-return)
 
+	Notes:
+	1. The handling of line continuations is quite poor (if I have
+	to say it myself).  I should have used a C++ |string(3c++|
+	object rather than the fixed (although quite large) buffer
+	that I am using.  In the end laziness about not changing
+	existing code rules the day (which practice I usually quite 
+	despised back in the 1980s).
+
 *******************************************************************************/
 
 module ;
@@ -46,6 +54,7 @@ module ;
 #include	<climits>		/* |INT_MAX| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
+#include	<string>
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
@@ -56,6 +65,7 @@ module ;
 #include	<langproc.h>
 #include	<ascii.h>
 #include	<hasx.h>		/* |hasmodname(3uc)| */
+#include	<strmgr.h>
 #include	<strop.h>
 #include	<strwcmp.h>
 #include	<mkchar.h>
@@ -63,13 +73,20 @@ module ;
 #include	<localmisc.h>
 
 #pragma		GCC dependency	"mod/modproc.ccm"
+#pragma		GCC dependency	"mod/ulibvals.ccm"
 
 module modproc ;
+
+import ulibvals ;			/* |ulibval(3u)| + |pagesz| */
 
 /* local defines */
 
 
 /* imported namespaces */
+
+using std::string ;			/* type */
+using libu::umemalloc ;			/* subroutine */
+using libu::umemfree ;			/* subroutine */
 
 
 /* local typedefs */
@@ -84,17 +101,30 @@ module modproc ;
 /* local structures */
 
 namespace {
-    struct vecmgr {
+    struct modmgr {
 	vecstr		*vop ;
+	char		*lbuf{} ;
 	langproc	proc ;
-	vecmgr(vecstr *p) noex : vop(p) { } ;
+	strmgr		lnmgr ;
+	int		llen = 0 ;
+	int		li = 0 ;
+	bool		lnalt = false ;
+	modmgr(vecstr *p) noex : vop(p) { } ;
 	int operator () (cchar *) noex ;
+	int start() noex ;
+	int finish() noex ;
 	int fileload(cchar *) noex ;
 	int fileproc() noex ;
 	int liner(int,size_t) noex ;
-	int loadln(int,cchar *,int) noex ;
+	int checkln(int,cchar *,int) noex ;
 	int procln(int,cchar *,int) noex ;
-    } ; /* end struct (vecmgr) */
+    private:
+	int lnbegin() noex ;
+	int lnend() noex ;
+	int lnready() noex ;
+	int lnadd(cchar *,int) noex ;
+	int lncheck(int) noex ;
+    } ; /* end struct (modmgr) */
 } /* end namespace */
 
 
@@ -115,7 +145,7 @@ int modprocload(vecstr *op,cchar *fname) noex {
     	int		rs = SR_FAULT ;
 	if_constexpr (f_debug) debprintf(__func__,"ent fn=%s\n",fname) ;
 	if (op && fname) ylikely {
-    	    vecmgr	mgr(op) ;
+    	    modmgr	mgr(op) ;
 	    rs = mgr(fname) ;
 	} /* end if (non-null) */
 	if_constexpr (f_debug) debprintf(__func__,"ret rs=%d\n",rs) ;
@@ -126,22 +156,34 @@ int modprocload(vecstr *op,cchar *fname) noex {
 
 /* local subroutines */
 
-int vecmgr::operator () (cchar *fname) noex {
+int modmgr::operator () (cchar *fname) noex {
     	int		rs ;
 	int		rs1 ;
 	int		rv = 0 ; /* return-value */
-	if ((rs = proc.start) >= 0) {
-	    if ((rs = fileload(fname)) >= 0) {
-		rs = fileproc() ;
-		rv = rs ;
-	    } /* end if (fileload) */
-	    rs1 = proc.finish ;
+	if ((rs = start()) >= 0) {
+	    if ((rs = proc.start) >= 0) {
+	        if ((rs = fileload(fname)) >= 0) {
+		    rs = fileproc() ;
+		    rv = rs ;
+	        } /* end if (fileload) */
+	        rs1 = proc.finish ;
+	        if (rs >= 0) rs = rs1 ;
+	    } /* end if (langproc) */
+	    rs1 = finish() ;
 	    if (rs >= 0) rs = rs1 ;
-	} /* end if (langproc) */
+	} /* end if (start-finish) */
     	return (rs >= 0) ? rv : rs ;
-} /* end method (vecmgr::operator) */
+} /* end method (modmgr::operator) */
 
-int vecmgr::fileload(cchar *fname) noex {
+int modmgr::start() noex {
+    	return SR_OK ;
+}
+
+int modmgr::finish() noex {
+    	return lnend() ;
+}
+
+int modmgr::fileload(cchar *fname) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		nmods = 0 ;
@@ -168,9 +210,9 @@ int vecmgr::fileload(cchar *fname) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? nmods : rs ;
-} /* end method (vecmgr::fileload) */
+} /* end method (modmgr::fileload) */
 
-int vecmgr::liner(int fd,csize ms) noex {
+int modmgr::liner(int fd,csize ms) noex {
 	cnullptr	np{} ;
 	cint		mp = PROT_READ ;
 	cint		mf = MAP_SHARED ;
@@ -188,17 +230,34 @@ int vecmgr::liner(int fd,csize ms) noex {
 		cchar	*lp = charp(md) ;
 		for (cchar *tp ; (tp = charp(memchr(lp,'\n',ll))) != np ; ) {
 		    csize	si = ((tp + 1) - lp) ;
-		    if (cint sl = intconv(si - 1) ; sl > 0) {
-		        rs = loadln(ln++,lp,sl) ;
-		        nmods += rs ;
-		    }
+		    if (int sl = intconv(si - 1) ; sl > 0) {
+			cchar *sp = lp ;
+	    		if (sp[sl - 1] == '\r') {
+			    sl -= 1 ;
+	    		}
+	    		if (sl > 0) {
+			    if (sp[sl - 1] == CH_BSLASH) {
+			        rs = lnadd(sp,(sl - 1)) ;
+			    } else {
+			        if (lnalt) {
+			            if ((rs = lnadd(sp,sl)) >= 0) {
+				        rs = lncheck(ln++) ;
+		                        nmods += rs ;
+				    }
+			        } else {
+		                    rs = checkln(ln++,sp,sl) ;
+		                    nmods += rs ;
+			        }
+			    } /* end if (continuation or regular) */
+			} /* end if (still not-empty) */
+		    } /* end if (non-empty) */
 		    ll -= si ;
 		    lp += si ;
 		    if (rs < 0) break ;
 		} /* end for */
 		if ((rs >= 0) && (ll > 0)) {
 		    cint	sl = intconv(ll) ;
-		    rs = loadln(ln++,lp,sl) ;
+		    rs = checkln(ln++,lp,sl) ;
 		    nmods += rs ;
 		} /* end if (trailing line */
 	    } /* end if (memory-advise) */
@@ -209,46 +268,58 @@ int vecmgr::liner(int fd,csize ms) noex {
 	    debprintf(__func__,"ret rs=%d c=%d\n",rs,nmods) ;
 	}
 	return (rs >= 0) ? nmods : rs ;
-} /* end method (vecmgr::liner) */
+} /* end method (modmgr::liner) */
 
-int vecmgr::loadln(int ln,cchar *lp,int ll) noex {
+int modmgr::checkln(int ln,cchar *lp,int ll) noex {
     	int		rs = SR_OK ;
 	if_constexpr (f_debug) {
 	    strnul s(lp,ll) ;
 	    debprintf(__func__,"ent ln=%d l=>%s<\n",ln,ccp(s)) ;
 	}
 	if ((ll > 0) && (lp[0] != '#')) {
-	    rs = proc.proc(ln,lp,ll) ;
-	    debprintf(__func__,"proc.proc() ret rs=%d\n",rs) ;
-	}
+	    if ((ll > 0) && (lp[ll - 1] == '\r')) {
+		ll -= 1 ;
+	    }
+	    if (ll > 0) {
+	        rs = proc.proc(ln,lp,ll) ;
+	        debprintf(__func__,"proc.proc() ret rs=%d\n",rs) ;
+	    }
+	} /* end if (not preprocessor directive) */
 	debprintf(__func__,"ret rs=%d\n",rs) ;
     	return rs ; 
-} /* end method (vecmgr::loadln) */
+} /* end method (modmgr::checkln) */
 
-int vecmgr::fileproc() noex {
+int modmgr::fileproc() noex {
     	cint		rsn = SR_NOTFOUND ;
     	int		rs ;
 	int		rs1{} ;
 	int		rv = 0 ; /* return-value */
 	debprintf(__func__,"ent\n") ;
 	if (langproc_cur cur ; (rs = proc.curbegin(&cur)) >= 0) {
-	    cchar	*lp ;
 	    int		ln = 1 ;
-	    while ((rs1 = proc.curenum(&cur,&lp)) >= 0) {
+	    debprintf(__func__,"while-cur\n") ;
+	    for (cchar *lp ; (rs1 = proc.curenum(&cur,&lp)) >= 0 ; ) {
+		cint ll = rs1 ;
 		if (lp) {
-		    rs = procln(ln++,lp,rs) ;
+		    {
+			strnul s(lp,ll) ;
+		        debprintf(__func__,"enum s=>%s<\n",ccp(s)) ;
+		    }
+		    rs = procln(ln++,lp,ll) ;
 		    rv += rs ;
 		}
-	    } /* end while (langproc_curenum) */
+		if (rs < 0) break ;
+	    } /* end for (langproc_curenum) */
+	    debprintf(__func__,"while-cur rs=%d rs1=%d\n",rs,rs1) ;
 	    if ((rs >= 0) && (rs1 != rsn)) rs = rs1 ;
 	    rs1 = proc.curend(&cur) ;
 	    if (rs >= 0) rs = rs1 ;
 	} /* end if (langproc_cur) */
 	debprintf(__func__,"ret rs=%d rv=%d\n",rs,rv) ;
     	return (rs >= 0) ? rv : rs ;
-} /* end method (vecmgr::fileproc) */
+} /* end method (modmgr::fileproc) */
 
-int vecmgr::procln(int ln,cchar *lp,int ll) noex {
+int modmgr::procln(int ln,cchar *lp,int ll) noex {
     	int		rs ;
 	int		rs1 ;
 	int		c = 0 ; /* return-value -- number of names found */
@@ -285,6 +356,76 @@ int vecmgr::procln(int ln,cchar *lp,int ll) noex {
 	} /* end if (strop) */
 	if_constexpr (f_debug) debprintf(__func__,"ret rs=%d c=%d\n",rs,c) ;
 	return (rs >= 0) ? c : rs ;
-} /* end method (vecmgr::procln) */
+} /* end method (modmgr::procln) */
+
+int modmgr::lnbegin() noex {
+    	int		rs = SR_OK ;
+	if (lbuf == nullptr) {
+	    if ((rs = ulibval.pagesz) >= 0) {
+		cint	sz = (4 * rs) ;
+		if (char *bp ; (rs = umemalloc(sz,&bp)) >= 0) {
+		    lbuf = bp ;
+		    llen = sz ;
+		    rs = lnmgr.start(lbuf,llen) ;
+		    if (rs < 0) {
+			umemfree(lbuf) ;
+			lbuf = nullptr ;
+			llen = 0 ;
+		    } /* end if (error) */
+		} /* end if (memory-allocation) */
+	    } /* end if (ulibvals) */
+	} /* end if (required) */
+	return rs ;
+} /* end method (modmgr::lnbegin) */
+
+int modmgr::lnend() noex {
+    	int		rs = SR_OK ;
+	int		rs1 ;
+	if (lbuf) {
+	    {
+		rs1 = lnmgr.finish ;
+		if (rs >= 0) rs = rs1 ;
+	    }
+	    {
+	        rs1 = umemfree(lbuf) ;
+	        if (rs >= 0) rs = rs1 ;
+	        lbuf = nullptr ;
+	        llen = 0 ;
+	    }
+	    li = 0 ;
+	} /* end if (was initialized) */
+	return rs ;
+} /* end method (modmgr::lnend) */
+
+int modmgr::lnready() noex {
+    	int		rs = SR_OK ;
+	if (lbuf == nullptr) {
+	    rs = lnbegin() ;
+	}
+	return rs ;
+} /* end method (modmgr::lnready) */
+
+int modmgr::lnadd(cchar *sp,int sl) noex {
+    	int		rs ;
+	if ((rs = lnready()) >= 0) {
+	    if ((rs = lnmgr.str(sp,sl)) >= 0) {
+	        lnalt = true ;
+	    } /* end if (lnadd) */
+	} /* end if (lnready) */
+	return rs ;
+} /* end method (modmgr::lnadd) */
+
+int modmgr::lncheck(int ln) noex {
+    	int		rs ;
+	if (cchar *cp ; (rs = lnmgr.get(&cp)) >= 0) {
+	    cint cl = rs ;
+	    if ((rs = checkln(ln,cp,cl)) >= 0) {
+	        if ((rs = lnmgr.reset) >= 0) {
+	            lnalt = false ;
+	        }
+	    } /* endif (checkln) */
+	} /* end if (lnmgr.get) */
+	return rs ;
+} /* end method (modmgr::lncheck) */
 
 
