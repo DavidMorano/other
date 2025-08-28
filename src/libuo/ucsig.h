deb80fd421c8256861d8c39103632aaa974c24eb
David A.D. Morano <david.a.morano@gmail.com>
2025-08-28 00:19:44 -0400

    restore

---
+++ src/libuo/ucsig.h
@@ -0,0 +1,35 @@
+/* ucsig HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 (conformance reviewed) */
+
+/* interface component for UNIX® library-3c */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCSIG_INCLUDE
+#define	UCSIG_INCLUDE
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+
+#include	<ucsigset.h>
+#include	<ucsigx.h>
+
+
+#endif /* UCSIG_INCLUDE */
+
+
