bad276e0aff2135cd44559d847db0e3fcbdf37b6
David A.D. Morano <david.a.morano@gmail.com>
2026-01-03 01:04:02 -0500

    restore

---
+++ src/libuo/pow.h
@@ -0,0 +1,35 @@
+/* pow HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* calculate the integer exponential of an integer */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	POW_INCLUDE
+#define	POW_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<stdintx.h>
+
+#include	<powtwo.h>
+#include	<xpow.h>
+
+
+#endif /* POW_INCLUDE */
+
+
