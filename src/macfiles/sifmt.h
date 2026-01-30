5246bed47cb36edbbd5e1c75293594b6221ead4d
David A.D. Morano <david.a.morano@gmail.com>
2026-01-30 17:02:26 -0500

    restore

---
+++ src/macfiles/sifmt.h
@@ -0,0 +1,10 @@
+
+#define S_IFIFO         0010000         /* [XSI] named pipe (fifo) */
+#define S_IFCHR         0020000         /* [XSI] character special */
+#define S_IFDIR         0040000         /* [XSI] directory */
+#define S_IFBLK         0060000         /* [XSI] block special */
+#define S_IFREG         0100000         /* [XSI] regular */
+#define S_IFLNK         0120000         /* [XSI] symbolic link */
+#define S_IFSOCK        0140000         /* [XSI] socket */
+#define S_IFWHT         0160000         /* OBSOLETE: whiteout */
+
