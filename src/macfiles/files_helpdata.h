c8f2fc73f8966a7f48e0e8532662db2d6368ad03
David A.D. Morano <david.a.morano@gmail.com>
2026-02-10 12:13:45 -0500

    restore

---
+++ src/macfiles/files_helpdata.h
@@ -0,0 +1,25 @@
+/* files_helpdata HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* help-usage data */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	HELPDATA_INCLUDE
+#define	HELPDATA_INCLUDE
+
+extern const cpcchar	helpdata[] ;
+
+#endif /* HELPDATA_INCLUDE */
+
+
