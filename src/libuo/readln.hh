17a710c4a3f1058c907878fe7593e9fa403b4c23
David A.D. Morano <david.a.morano@gmail.com>
2026-03-19 00:10:21 -0400

    update

--- src/libuo/readln.hh
+++ src/libuo/readln.hh
@@ -23,14 +23,12 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<istream>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 extern int readln(std::istream *,char *,int,int = 0) noex ;
 extern int istr_readln(std::istream *,char *,int,int = 0) noex ;
+
 
 #endif /* __cplusplus */
 #endif /* READLN_INCLUDE */
