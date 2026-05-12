7ac64422427ff087f7fd8048a7660d347b1b5f94
David A.D. Morano <david.a.morano@gmail.com>
2026-05-12 01:41:22 -0400

    restore

---
+++ src/macfiles/files_show.h
@@ -0,0 +1,36 @@
+/* files_show HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* calculate the number of lines in a file */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2023-12-17, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2023 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FILESSHOW__INCLUDE
+#define	FILESSHOW__INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	files_show(cchar *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* FILESSHOW__INCLUDE */
+
+
