ea24b35a1486265bc661c32b0fcc23f5f4bac852
David A.D. Morano <david.a.morano@gmail.com>
2026-07-01 00:25:14 -0400

    restore

---
+++ src/macfiles/files_conf.h
@@ -0,0 +1,29 @@
+/* files_conf HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* calculate the number of lines in a file */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2023-12-17, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2023 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FILESCONF__INCLUDE
+#define	FILESCONF__INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* FILESCONF__INCLUDE */
+
+
