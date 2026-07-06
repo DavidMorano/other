79cfe4efba1cbcec8a63bb1208adccce58bb834f
David A.D. Morano <david.a.morano@gmail.com>
2026-07-06 00:06:16 -0400

    update

--- src/libuo/field.h
+++ src/libuo/field.h
@@ -35,12 +35,12 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<vecstr.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<vecstr.h>		/* LIBUC */
 
 
 #define	FIELD		struct field_head
