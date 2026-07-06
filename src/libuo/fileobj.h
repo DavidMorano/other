87b3dc5604820b4db4574b10e09baacc153597d0
David A.D. Morano <david.a.morano@gmail.com>
2026-07-06 00:08:47 -0400

    restore

---
+++ src/libuo/fileobj.h
@@ -0,0 +1,34 @@
+/* fileobj HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* FILEOBJ utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This file was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FILEOBJ_INCLUDE
+#define	FILEOBJ_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysnase.h>
+
+#include	<filegrp.h>
+#include	<filelinker.hh>
+#include	<filemap.h>
+#include	<filesyner.hh>
+
+
+#endif /* FILEOBJ_INCLUDE */
+
+
