5fd2fc00936d5d92baf9eccd7a05a7c384094171
David A.D. Morano <david.a.morano@gmail.com>
2026-08-31 01:05:53 -0400

    restore

---
+++ src/tzabbr/tzabbr_main.cc
@@ -0,0 +1,110 @@
+/* tzabbr_main SUPPORT (tzabbr) */
+/* charset=ISO8859-1 */
+/* lang=C++23 */
+
+/* print out the abbreviation of the curreent time-zone */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-04-11, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	main (tzabbr)
+
+	Description:
+	This program prints out the abbreviation of the curreent
+	time-zone.
+
+	Synopsis:
+	$ tzabbr
+
+	Arguments:
+	-		none
+
+	Returns:
+	EXIT_SUCCESS	OK
+	EXIT_FAILURE	some problem
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<termios.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<ctime>			/* CSTD */
+#include	<cerrno>		/* CSTD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<new>			/* C++STD |nullptr_t| */
+#include	<iostream>		/* C++STD */
+#include	<iomanip>		/* C++STD */
+
+
+/* local defines */
+
+#ifndef	noex
+#define	noex		noexcept
+#endif
+
+#ifndef	repeat
+#define repeat		do
+#define	until(cond)	while(!(cond))
+#endif
+
+#ifndef	SR_NOMEM
+#define	SR_NOMEM	(-ENOMEM)
+#endif
+
+#ifndef	SR_INTR
+#define	SR_INTR		(-EINTR)
+#endif
+
+
+/* imported namespaces */
+
+using std::cout ;			/* variable */
+
+
+/* local typedefs */
+
+typedef const char *const *mainv ;
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local constants */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int main(int,mainv,mainv) {
+	const time_t	dt = time(nullptr) ;
+	const nullptr_t	np{} ;;
+	int		ex = EXIT_FAILURE ;
+	struct tm	*tmp ;
+	if (struct tm tmd ; (tmp = localtime_r(&dt,&tmd)) != np) {
+	    cout << tmp->tm_zone << '\n' ;
+	    ex = EXIT_SUCCESS ;
+	} /* end if (localtime_t) */
+	return ex ;
+} /* end subroutine (main) */
+
+
