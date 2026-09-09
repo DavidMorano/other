00bdcc232e4565f88f0e5288b5ac49b61ddb5d2c
David A.D. Morano <david.a.morano@gmail.com>
2026-09-09 01:16:30 -0400

    restore

---
+++ src/marker/marker_main.cc
@@ -0,0 +1,84 @@
+/* marker_main SUPPORT (marker) */
+/* charset=ISO8859-1 */
+/* lang=C++23 */
+
+/* undetermined */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-04-11, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	main (marker)
+
+	Description:
+	the purpose of this program is undermined.
+
+	Synopsis:
+	$ marker
+
+	Arguments:
+	-		none
+
+	Returns:
+	EXIT_SUCCESS	OK
+	EXIT_FAILURE	some problem
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<termios.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<ctime>			/* CSTD */
+#include	<cerrno>		/* CSTD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<iostream>		/* C++STD */
+#include	<iomanip>		/* C++STD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+using std::cout ;			/* variable */
+
+
+/* local typedefs */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local constants */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int main(int,con mainv,con mainv) {
+    	return EXIT_SUCCESS ;
+} /* end subroutine (main) */
+
+
