346348ecac3846b10ecedbd722e79fdb54dbfb92
David A.D. Morano <david.a.morano@gmail.com>
2026-09-16 00:54:34 -0400

    update

--- src/rmsubpat/rmsubpat_main.cc
+++ src/rmsubpat/rmsubpat_main.cc
@@ -22,15 +22,17 @@
 	rmsubpat
 
 	Description:
-	This program removes (deletes) files within the given
-	directories that prefix-match on the given pattern.
+	This program is used to remove files -- that contain a given
+	pattern -- from the specified directories.  If no patterns
+	are given, patterns are determined (if present at all) from
+	the current directory from files that are C++ source modules.
 
 	Synopsis:
-	$ rmsubpat [-d <dir(s)>] <pattern(s)> 
+	$ rmsubpat [-d <dir(s)>] [<pattern(s)>]
 
 	Arguments:
-	<dir(s)>	directories to search
-	<pattern(s)>	pattern(s) to match on
+	<dir(s)>	optional directories to search
+	<pattern(s)>	optional pattern(s) to match on
 
 	Returns:
 	EXIT_SUCCESS	OK
@@ -41,7 +43,7 @@
 #include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD |EXIT_FAILURE| */
-#include	<cstring>		/* CSTD |strncmp(3c)| */
+#include	<cstring>		/* CSTD |strncmp(3c)| + |strstr(3c)| */
 #include	<exception>		/* C++STD */
 #include	<filesystem>		/* C++STD */
 #include	<clanguage.h>		/* LIBU */
@@ -67,6 +69,10 @@ import ureserve ;			/* |vecstr(3u)| */
 
 using namespace	std::filesystem ;	/* namespace */
 
+using std::error_code ;			/* type */
+using libu::siext ;			/* subroutine */
+using libu::hasext ;			/* subroutine */
+
 
 /* local typedefs */
 
@@ -87,11 +93,12 @@ namespace {
 	int		argc ;
 	dirmgr(int c,con mainv v) noex : argc(c), argv(v) { } ;
 	operator int () noex ;
-	int findpats	() noex ;
-	int patsload	(vecstr *) noex ;
-	int checkargs	() noex ;
-	int checkdirs	(cpcchar *,cchar *) noex ;
+	int checkargs	(cpccharp) noex ;
+	int checkdefs	(cpccharp) noex ;
+	int checkdirs	(cpccharp,cchar *) noex ;
 	int check	(cchar *,cchar *) noex ;
+	int defload	(vecstr *) noex ;
+	int defcheck	(cpccharp,vecstr *) noex ;
     } ; /* end struct (dirmgr) */
 } /* end namespace */
 
@@ -106,6 +113,7 @@ constexpr cpcchar	dirs[] = {
     nullptr
 } ; /* end array */
 
+cchar			defext[]	= "ccm" ;
 cbool			f_debug		= CF_DEBUG ;
 
 
@@ -118,7 +126,7 @@ int main(int argc,con mainv argv,con mainv) {
 	int		ex = EX_OK ;
 	int		rs = SR_OK ;
 	DPRINTF("ent\n") ;
-	if (argc > 1) {
+	if (argc >= 0) {
 	    dirmgr dm(argc,argv) ;
 	    rs = dm ;
 	} /* end if (arguments) */
@@ -137,24 +145,24 @@ dirmgr::operator int () noex {
 	int		c = 0 ;
 	DPRINTF("ent\n") ;
 	if (argc > 1) {
-	    rs = checkargs() ;
+	    rs = checkargs(dirs) ;
 	    c += rs ;
 	} else {
-	    rs = checkfinds() ;
+	    rs = checkdefs(dirs) ;
 	    c += rs ;
 	} /* end if (arguments or not) */
 	DPRINTF("ret rs=%d c=%d\n",rs,c) ;
 	return rs ;
 } /* end method (dirmgr::operator) */
 
-int dirmsg::checkargs() noex {
+int dirmgr::checkargs(cpccharp dirpp) noex {
     	int		rs = SR_OK ;
 	int		c = 0 ;
 	DPRINTF("ent\n") ;
 	    for (int ai = 1 ; (ai < argc) && argv[ai] ; ai += 1) {
 	        if (cchar *ap = argv[ai] ; ap[0]) {
 		    DPRINTF("ap=%s\n",ap) ;
-		    rs = checkdirs(dirs,ap) ;
+		    rs = checkdirs(dirpp,ap) ;
 		    c += rs ;
 	        } /* end if (non-empty) */
 	        if (rs < 0) break ;
@@ -163,29 +171,69 @@ int dirmsg::checkargs() noex {
 	return (rs >= 0) ? c : rs ;
 } /* end method (dirmgr::checkargs) */
 
-int dirmsg::checkfinds() noex {
+int dirmgr::checkdefs(cpccharp dirpp) noex {
     	int		rs = SR_OK ;
 	int		rs1 ;
 	int		c = 0 ;
 	DPRINTF("ent\n") ;
-	if (vecstr pats ; (rs = pats.start) >= 0) {
-	    if ((rs = patsload(&pats)) > 0) {
-		rs = patscheck(&pats) ;
+	if (vecstr pats ; (rs = pats.start) >= 0) ylikely {
+	    if ((rs = defload(&pats)) > 0) ylikely {
+		rs = defcheck(dirpp,&pats) ;
 		c += rs ;
-	    } /* end if (patsload) */
+	    } /* end if (defload) */
 	    rs1 = pats.finish ;
 	    if (rs >= 0) rs = rs1 ;
 	} /* end if (vecstr) */
 	DPRINTF("ret rs=%d c=%d\n",rs,c) ;
 	return (rs >= 0) ? c : rs ;
-} /* end method (dirmgr::checkfinds) */
+} /* end method (dirmgr::checkdefs) */
 
-int dirmgr::patsload(vecstr *plp) noex {
+int dirmgr::defload(vecstr *plp) noex {
 	int		rs = SR_OK ;
 	int		c = 0 ;
-
+	DPRINTF("ent\n") ;
+	error_code ec ;
+	if (path cur = current_path(ec) ; (! ec)) ylikely {
+	    for (dirit dir(cur) ; cauto &e : dir) {
+                if (con path &p = e.path() ; e.is_regular_file()) ylikely {
+                    con path bn = p.filename() ;
+                    if (cpcchar bns = bn.c_str() ; bns[0]) ylikely {
+			DPRINTF("bns=%s\n",bns) ;
+		        if (cint si = siext(bns,-1) ; si >= 0) {
+			    DPRINTF("si=%d ext=%s\n",si,(bns+si)) ;
+			    if (strcmp((bns+si),defext) == 0) {
+			        rs = plp->add(bns,(si - 1)) ;
+			        c += 1 ;
+			    } /* end if (strcmp) */
+		        } /* end if (siext) */
+		    } /* end if (get c-string) */
+                } /* end if (regular-file) */
+	        if (rs < 0) break ;
+            } /* end for */
+	} else {
+	    rs = (neg ec.value()) ;
+	} /* end if (path) */
+	DPRINTF("ret rs=%d c=%d\n",rs,c) ;
 	return (rs >= 0) ? c : rs ;
-} /* end method (dirmgr::patsload) */
+} /* end method (dirmgr::defload) */
+
+int dirmgr::defcheck(cpccharp dirpp,vecstr *plp) noex {
+	int		rs = SR_OK ;
+	int		c = 0 ;
+	DPRINTF("ent\n") ;
+	cchar *pp ;
+	for (int i = 0 ; plp->get(i,&pp) >= 0 ; i += 1) {
+	    if (pp) {
+		if (pp[0]) {
+		    rs = checkdirs(dirpp,pp) ;
+		    c += rs ;
+		} /* end if (non-empty) */
+	    } /* end */
+	    if (rs < 0) break ;
+	} /* end for */
+	DPRINTF("ret rs=%d c=%d\n",rs,c) ;
+	return (rs >= 0) ? c : rs ;
+} /* end method (dirmgr::defcheck) */
 
 int dirmgr::checkdirs(cpcchar *dirpp,cchar *patp) noex {
 	int		rs = SR_OK ;
@@ -209,14 +257,13 @@ int dirmgr::checkdirs(cpcchar *dirpp,cchar *patp) noex {
 } /* end method (dirmgr::checkdirs) */
 
 int dirmgr::check(cchar *dn,cchar *patp) noex {
-    	cint		patl = lenstr(patp) ;
 	int		rs = SR_OK ;
 	int		c = 0 ;
 	DPRINTF("ent\n") ;
 	for (dirit dir(dn) ; cauto &e : dir) {
             if (con path &p = e.path() ; e.is_regular_file()) {
                 con path bn = p.filename() ;
-                if (cchar *bns = bn.c_str() ; strncmp(bns,patp,patl) == 0) {
+                if (cchar *bns = bn.c_str() ; strstr(bns,patp)) {
 		    DPRINTF("prefix-match ps=%s\n",bns) ;
 		    if (cchar *fn = p.c_str() ; fn[0]) {
 		        DPRINTF("prefix-match file-path=%s\n",fn) ;
